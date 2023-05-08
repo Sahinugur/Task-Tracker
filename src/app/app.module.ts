c4dcc4601854f27e61009eeecae578696b5ffd51
Sahinugur <ugurlao@gmail.com>
2023-05-08 18:21:21 +0200

    tasks created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -12,8 +14,9 @@ import { TasksComponent } from './components/tasks/tasks.component';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
+    TaskItemComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { TASKS } from '../../mock-tasks';
+import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
 
 @Component({
@@ -8,5 +8,17 @@ import { Task } from '../../Task';
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe(
+      (tasks: Task[]) => {
+        this.tasks = tasks;
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  }
 }
